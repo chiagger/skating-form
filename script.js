35a24c99f22e40f0332730a24a803004d33370a2
chiagger <busca.chiara.cb@gmail.com>
2022-11-08 17:48:07 +0100

    added js matching passwords form validation

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const pssw = document.querySelector("#password");
+const psswConf = document.querySelector("#password-conf");
 
+
+psswConf.addEventListener("input", () => {
+    if (psswConf.value === pssw.value) {
+        psswConf.setCustomValidity("");
+    }
+    if (psswConf.value != pssw.value) {
+        psswConf.setCustomValidity("passwords do not match");
+    }
+})
